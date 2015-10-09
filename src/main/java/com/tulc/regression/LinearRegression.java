db6adac9950339fca3402de06dd9c5bb23bf7129
Tulasi Paradarami <research800@gmail.com>
2015-10-09 14:05:46 -0700

    template for lin regression

---
+++ src/main/java/com/tulc/regression/LinearRegression.java
@@ -0,0 +1,14 @@
+package com.tulc.regression;
+
+import java.util.Vector;
+
+import com.tulc.base.BaseModel;
+import com.tulc.math.Matrix;
+
+public class LinearRegression extends BaseModel {
+
+	public LinearRegression(Matrix X, Vector y) {
+		super(X, y);
+	}
+
+}
