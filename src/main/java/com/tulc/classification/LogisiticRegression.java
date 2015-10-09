5669b05baa938bfbe5597cd9923b9c20a966156f
Tulasi Paradarami <research800@gmail.com>
2015-10-09 14:05:26 -0700

    template for logistic regression

---
+++ src/main/java/com/tulc/classification/LogisiticRegression.java
@@ -0,0 +1,14 @@
+package com.tulc.classification;
+
+import java.util.Vector;
+
+import com.tulc.base.BaseModel;
+import com.tulc.math.Matrix;
+
+public class LogisiticRegression extends BaseModel {
+
+	public LogisiticRegression(Matrix X, Vector y) {
+		super(X, y);
+	}
+
+}
