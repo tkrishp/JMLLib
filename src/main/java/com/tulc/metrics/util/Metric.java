3a7ffe57671b81bc99204343363ed20bcb355048
Tulasi Paradarami <research800@gmail.com>
2015-10-23 13:15:27 -0700

    calculate mse & rmse

---
+++ src/main/java/com/tulc/metrics/util/Metric.java
@@ -0,0 +1,24 @@
+package com.tulc.metrics.util;
+
+import java.io.IOException;
+import java.util.Vector;
+
+public class Metric {
+    public static double MSE(Vector yActual, Vector yPred) throws IOException {
+        double mse = 0.0;
+        Vector<Double> ya = yActual;
+        Vector<Double> yp = yPred;
+        
+        if (yActual.size() != yPred.size())
+            throw new IOException("Vectors are unequal size");
+        
+        for (int i = 0; i < ya.size(); i++) {
+            mse += (ya.get(i) - yp.get(i)) * (ya.get(i) - yp.get(i));
+        }
+        return (mse/ya.size());
+    }
+    
+    public static double RMSE(Vector yActual, Vector yPred) throws IOException {
+        return Math.sqrt(MSE(yActual, yPred));
+    }
+}
