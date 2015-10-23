041b8678876e7882cb53acd7d42b01217fc4203d
Tulasi Paradarami <research800@gmail.com>
2015-10-23 16:02:00 -0700

    sgd has 1 iteration only

--- src/main/java/com/tulc/optimization/SGD.java
+++ src/main/java/com/tulc/optimization/SGD.java
@@ -1,11 +1,12 @@
 package com.tulc.optimization;
 
+import java.io.IOException;
 import java.util.Vector;
 
 import com.tulc.math.Matrix;
 
 public class SGD extends GradientDescent {
-    public SGD(Double iniTheeta, Matrix<Double> X, Vector<Double> y, Double mseGain) {
-        super(iniTheeta, X, y, 1, mseGain);
+    public SGD(Double iniTheeta, Matrix<Double> X, Vector<Double> y, Double mseGain) throws IOException {
+        super(iniTheeta, X, y, 1);
     }
 }
