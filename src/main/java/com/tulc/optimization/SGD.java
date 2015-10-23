5be980fce136a305473e0b55dab75b60b1fec027
Tulasi Paradarami <research800@gmail.com>
2015-10-23 16:03:32 -0700

    correct constructor params

--- src/main/java/com/tulc/optimization/SGD.java
+++ src/main/java/com/tulc/optimization/SGD.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 import com.tulc.math.Matrix;
 
 public class SGD extends GradientDescent {
-    public SGD(Double iniTheeta, Matrix<Double> X, Vector<Double> y, Double mseGain) throws IOException {
+    public SGD(Double iniTheeta, Matrix<Double> X, Vector<Double> y) throws IOException {
         super(iniTheeta, X, y, 1);
     }
 }
