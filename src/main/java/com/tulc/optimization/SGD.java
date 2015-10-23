51a866858512d6b834587a520c7194c07eaf40e7
Tulasi Paradarami <research800@gmail.com>
2015-10-23 15:16:58 -0700

    initial version of sgd

--- src/main/java/com/tulc/optimization/SGD.java
+++ src/main/java/com/tulc/optimization/SGD.java
@@ -1,5 +1,11 @@
 package com.tulc.optimization;
 
-public class SGD extends GradientDescent {
+import java.util.Vector;
 
+import com.tulc.math.Matrix;
+
+public class SGD extends GradientDescent {
+    public SGD(Double iniTheeta, Matrix<Double> X, Vector<Double> y, Double mseGain) {
+        super(iniTheeta, X, y, 1, mseGain);
+    }
 }
