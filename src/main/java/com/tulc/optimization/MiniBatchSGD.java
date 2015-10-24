cb7406b03999c0c899ca467ceac32fc4f25db996
Tulasi Paradarami <research800@gmail.com>
2015-10-23 18:33:25 -0700

    generics in mini bactch sgd

--- src/main/java/com/tulc/optimization/MiniBatchSGD.java
+++ src/main/java/com/tulc/optimization/MiniBatchSGD.java
@@ -5,8 +5,8 @@ import java.util.Vector;
 
 import com.tulc.math.Matrix;
 
-public class MiniBatchSGD extends GradientDescent {
-    public MiniBatchSGD(Double iniTheeta, Matrix<Double> X, Vector<Double> y, GradientDescentOptions gdo) throws IOException {
+public class MiniBatchSGD <E extends Number >extends GradientDescent<E> {
+    public MiniBatchSGD(Double iniTheeta, Matrix<E> X, Vector<E> y, GradientDescentOptions gdo) throws IOException {
         super(iniTheeta, X, y, gdo);
     }
 }
