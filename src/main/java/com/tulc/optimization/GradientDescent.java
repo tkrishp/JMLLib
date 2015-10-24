57124ad076c1c5035f01ccfe5cf83d4964d762df
Tulasi Paradarami <research800@gmail.com>
2015-10-23 18:31:28 -0700

    use generics where possible

--- src/main/java/com/tulc/optimization/GradientDescent.java
+++ src/main/java/com/tulc/optimization/GradientDescent.java
@@ -3,9 +3,7 @@ package com.tulc.optimization;
 import java.io.IOException;
 import java.util.Vector;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import com.tulc.math.GenericTypeOp;
 import com.tulc.math.Matrix;
 import com.tulc.math.MatrixUtils;
 
@@ -14,16 +12,16 @@ import com.tulc.math.MatrixUtils;
  * test more
  */
 public class GradientDescent<E extends Number> {
-    private Vector<Double> theeta;
-    private Vector<Double> y;
-    private Matrix<Double> x;
+    private Vector<E> theeta;
+    private Vector<E> y;
+    private Matrix<E> x;
     private Vector<Double> loss;
     private int numOfIter = 0;
     private Double mseGain = 0.0;
     private Integer numOfRows;
     private Integer numOfFeatures;
     private Double mse;
-    private MatrixUtils<Double> matUtil;
+    private MatrixUtils<E> matUtil;
     private boolean checkNumOfIter;
     private boolean checkMseGain;
     
@@ -44,14 +42,14 @@ public class GradientDescent<E extends Number> {
     @SuppressWarnings("unchecked")
     public GradientDescent(Double iniTheeta, Matrix<E> x, Vector<E> y, GradientDescentOptions gdo) 
             throws IOException {
-        matUtil = new MatrixUtils<Double>();
-        theeta = new Vector<Double>();
+        matUtil = new MatrixUtils<E>();
+        theeta = new Vector<E>();
         this.theeta.setSize(x.numOfCols());
         for (int i=0; i<this.theeta.size(); i++) {
-            this.theeta.set(i, iniTheeta);
+            this.theeta.set(i, (E) iniTheeta);
         }
-        this.x = (Matrix<Double>) x;
-        this.y = (Vector<Double>) y;
+        this.x = x;
+        this.y = y;
         this.numOfIter = gdo.getNumOfIter();
         this.mseGain = gdo.getMseGain();
         this.numOfRows = x.numOfRows();
@@ -68,25 +66,25 @@ public class GradientDescent<E extends Number> {
      * Run the gradient descent algorithm till threshold conditions are satisfied
      * @throws IOException
      */
+    @SuppressWarnings("unchecked")
     public Vector<Double> optimize() throws IOException {
         if (! (checkNumOfIter || checkMseGain))
             throw new IOException("Gradient descent must have a defined exit condition. " +
                     "Provide number of iterations or mse gain");
         Double gradient = new Double(0);
         Double alpha = 0.001;
-        Vector<Double> newTheeta = new Vector<Double>();
         Double prevMse = (double) 0;
-        newTheeta = new Vector<Double>(numOfFeatures);
+        Vector<E> newTheeta = new Vector<E>(numOfFeatures);
         Double step = (double) 0;
         
         int i = 0;
         do {
             computeLossAndMse();
             for (int m=0; m<numOfFeatures; m++) {
-                gradient = sumOfElements(matUtil.multiply(x.transpose(), loss));
-                step = gradient * alpha/numOfRows;
-                newTheeta.set(m, (theeta.get(m) - step));
-                gradient = (double) 0;
+                gradient = (Double) sumOfElements(matUtil.multiply(x.transpose(), (Vector<E>)loss));
+                step = (Double) GenericTypeOp.multiply(gradient, alpha)/numOfRows;
+                newTheeta.set(m, (E) GenericTypeOp.subtract(theeta.get(m), step));
+                gradient = 0.0;
             }
 
             theeta = newTheeta;
@@ -111,19 +109,20 @@ public class GradientDescent<E extends Number> {
      * @throws IOException
      */
     
+    @SuppressWarnings("unchecked")
     private void computeLossAndMse() throws IOException {
         Double yhat = new Double(0);
         loss = new Vector<Double>(numOfRows);
-
+        Vector<E> row = new Vector<E>();
         Double currY = new Double(0);
         
         if (numOfFeatures != theeta.size())
             throw new IOException();
         
         for (int n=0; n<numOfRows; n++) {
-            Vector<Double> row = x.getRow(n);
-            yhat = matUtil.multiply(row, theeta);
-            currY = y.get(n);
+            row = x.getRow(n);
+            yhat = (Double) matUtil.multiply(row, theeta);
+            currY = (Double) y.get(n);
             loss.set(n, (yhat - currY));
             mse += (yhat - currY) * (yhat - currY);
         }
@@ -134,14 +133,15 @@ public class GradientDescent<E extends Number> {
      * Returns the vector of theeta
      * @return
      */
+    @SuppressWarnings("unchecked")
     public Vector<Double> getTheeta() {
-        return theeta;
+        return (Vector<Double>) theeta;
     }
     
-    private Double sumOfElements(Vector<Double> a) {
-        Double ret = (double) 0.0;
+    private Double sumOfElements(Vector<E> a) {
+        Double ret = new Double(0.0);
         for (int i = 0; i < a.size(); i++) {
-            ret += a.get(i);
+            ret += (Double) a.get(i);
         }
         return ret;
     }
