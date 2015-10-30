84ee888f56c43d095a91236598a0e5bd62cad9eb
Tulasi Paradarami <research800@gmail.com>
2015-10-30 11:21:41 -0700

    updates to sgd

--- src/main/java/com/tulc/optimization/SGD.java
+++ src/main/java/com/tulc/optimization/SGD.java
@@ -10,5 +10,10 @@ public class SGD <E extends Number> extends GradientDescent<E> {
         super();
         GradientDescentOptions gdo = new GradientDescentOptions();
         gdo.setNumOfIter(1);
+        Vector<E> row = new Vector<E>();
+        for (int i = 0; i < X.numOfRows(); i++) {
+            row = X.getRow(i);
+            Matrix<E> rowMatrix = new Matrix<E>(1, X.numOfCols());
+        }
     }
 }
