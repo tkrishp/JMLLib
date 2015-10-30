5baead37b29f3f9db4811a10f172c98e2ba136a0
Tulasi Paradarami <research800@gmail.com>
2015-10-30 14:54:27 -0700

    build y-vector for sgd

--- src/main/java/com/tulc/optimization/SGD.java
+++ src/main/java/com/tulc/optimization/SGD.java
@@ -14,6 +14,15 @@ public class SGD <E extends Number> extends GradientDescent<E> {
         for (int i = 0; i < X.numOfRows(); i++) {
             row = X.getRow(i);
             Matrix<E> rowMatrix = new Matrix<E>(1, X.numOfCols());
+            Vector<E> yValue = new Vector<E>(1);
+            for (int j = 0; j < X.numOfCols(); j++) {
+                rowMatrix.insert(X.get(1, j), 1, j);
+                yValue.insertElementAt(y.get(i), 0);
+                super.X = rowMatrix;
+                super.y = yValue;
+                optimize();
+            }
+
         }
     }
 }
