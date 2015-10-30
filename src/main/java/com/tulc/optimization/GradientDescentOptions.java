9a9f28c264d70a6e94bfd05fc0f39d98ef68d4a8
Tulasi Paradarami <research800@gmail.com>
2015-10-30 10:12:42 -0700

    constructor for grad descent options

--- src/main/java/com/tulc/optimization/GradientDescentOptions.java
+++ src/main/java/com/tulc/optimization/GradientDescentOptions.java
@@ -1,9 +1,19 @@
 package com.tulc.optimization;
 
 public class GradientDescentOptions {
-    private Integer numOfIter = -1;
-    private double mseGain = -1;
+    private final Integer DEF_NUM_ITER = 1;
+    private final Double DEF_MSE_GAIN = 0.01;
+    private final Double DEF_LEARN_RATE = 0.01;
+    
+    private Integer numOfIter;
+    private double mseGain;
     private double alpha;
+    
+    public GradientDescentOptions() {
+        numOfIter = DEF_NUM_ITER;
+        mseGain = DEF_MSE_GAIN;
+        alpha = DEF_LEARN_RATE;
+    }
     
     public void setNumOfIter(Integer n) {
         numOfIter = n;
