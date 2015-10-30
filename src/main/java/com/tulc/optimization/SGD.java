0cabaa3f301d6fbd23c674c3bea10fa4570c10f8
Tulasi Paradarami <research800@gmail.com>
2015-10-30 15:20:56 -0700

    updates to sgd constructor

--- src/main/java/com/tulc/optimization/SGD.java
+++ src/main/java/com/tulc/optimization/SGD.java
@@ -10,18 +10,14 @@ public class SGD <E extends Number> extends GradientDescent<E> {
         super();
         GradientDescentOptions gdo = new GradientDescentOptions();
         gdo.setNumOfIter(1);
-        Vector<E> row = new Vector<E>();
+        super.X = new Matrix<E>(1, X.numOfCols());
+        super.y = new Vector<E>(1);
         for (int i = 0; i < X.numOfRows(); i++) {
-            row = X.getRow(i);
-            Matrix<E> rowMatrix = new Matrix<E>(1, X.numOfCols());
-            Vector<E> yValue = new Vector<E>(1);
+            super.y.insertElementAt(y.get(i), 0);
             for (int j = 0; j < X.numOfCols(); j++) {
-                rowMatrix.insert(X.get(1, j), 1, j);
-                yValue.insertElementAt(y.get(i), 0);
-                super.X = rowMatrix;
-                super.y = yValue;
-                optimize();
+                super.X.insert(X.get(i, j), 1, j);
             }
+            optimize();
         }
     }
 }
