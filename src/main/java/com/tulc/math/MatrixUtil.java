3935f763c92a02f8c20ccc9bb3ad2d0e7769e46f
Tulasi Paradarami <research800@gmail.com>
2015-11-20 18:26:15 -0800

    new method to scale a vector

--- src/main/java/com/tulc/math/MatrixUtil.java
+++ src/main/java/com/tulc/math/MatrixUtil.java
@@ -6,27 +6,27 @@ import java.util.Vector;
 public class MatrixUtil {
     public static Double dotProduct(Vector<Double> a, Vector<Double> b) throws IOException {
         Double ret = new Double(0.0);
-        if (a.size() != b.size())
+        if (a.capacity() != b.capacity())
             throw new IOException("Cannot perform scalar multiplication on vectors of different sizes");
-        for (int i = 0; i < a.size(); i++) {
+        for (int i = 0; i < a.capacity(); i++) {
             ret += a.get(i) * b.get(i);
         }
         return ret;
     }
     
     public static Vector<Double> eleWiseMultiply(Vector<Double> a, Vector<Double> b) throws IOException {
-        if (a.size() != b.size()) {
+        if (a.capacity() != b.capacity()) {
             throw new IOException("Cannot perform element-wise multiplication on vectors of different sizes");
         }
-        Vector<Double> ret = new Vector<Double>(a.size());
-        for(int i = 0; i < a.size(); i++) {
+        Vector<Double> ret = new Vector<Double>(a.capacity());
+        for(int i = 0; i < a.capacity(); i++) {
             ret.insertElementAt(a.get(i) * b.get(i), i);
         }
         return ret;
     }
     
     public static Vector<Double> subtract(Vector<Double> a, Vector<Double> b) throws IOException {
-        if (a.size() != b.size()) {
+        if (a.capacity() != b.capacity()) {
             throw new IOException("Cannot perform element-wise subtraction on vectors of different sizes");
         }
         Vector<Double> ret = new Vector<Double>(a.capacity());
@@ -42,5 +42,13 @@ public class MatrixUtil {
             uv.set(i, 1d);
         }
         return uv;
+    }
+    
+    public static Vector<Double> scaleVector(double scale, Vector<Double> a) {
+        Vector<Double> ret = new Vector<Double>(a.capacity());
+        for (int i = 0; i < ret.capacity(); i++) {
+            ret.insertElementAt(a.get(i) * scale, i);
+        }
+        return ret;
     }
 }
