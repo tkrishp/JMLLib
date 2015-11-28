8f0a53c7f44c517f75e48bcb9fa3f15b4c0f11b9
Tulasi Paradarami <research800@gmail.com>
2015-11-28 15:42:56 -0800

    provide a real vector class

---
+++ src/main/java/com/tulc/math/RVector.java
@@ -0,0 +1,14 @@
+package com.tulc.math;
+
+import java.util.Vector;
+
+public class RVector extends Vector<Double> {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4506477794064248264L;
+
+    public RVector() {
+        
+    }
+}
