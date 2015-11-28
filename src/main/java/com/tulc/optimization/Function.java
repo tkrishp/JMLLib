8df92a6308e44c9b4297c40acd09329b054d72b9
Tulasi Paradarami <research800@gmail.com>
2015-11-28 15:43:26 -0800

    add abstract class Function

---
+++ src/main/java/com/tulc/optimization/Function.java
@@ -0,0 +1,14 @@
+package com.tulc.optimization;
+
+import com.tulc.math.RVector;
+
+/* 
+ * An abstract class for functions that can have a derivative
+ * A function's derivative is its gradient that is used in computing
+ * loss. Any class that extends function must implement its first
+ * derivative
+ */
+public abstract class Function {
+    protected double gradient = 0d;
+    abstract public double firstDerivative(RVector row, RVector theeta);
+}
