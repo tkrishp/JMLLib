15dc2e0673c67ebabe586c9b5c6e01d2c38c4499
Tulasi Paradarami <research800@gmail.com>
2015-12-11 09:29:52 -0800

    method calculate norm of a vector

--- src/main/java/com/tulc/math/RVector.java
+++ src/main/java/com/tulc/math/RVector.java
@@ -16,4 +16,19 @@ public class RVector extends Vector<Double> {
     public RVector(int r) {
         super(r);
     }
+    
+    /**
+     * Returns the p-norm of the vector. 
+     * if p=1, Manahatten distance is returned (L1/Lasso)
+     * if p=2, Euclidean distance is returned (L2/Ridge)
+     * @param p p-norm of the vector
+     * @return norm of the vector
+     */
+    public Double pNorm(int p) {
+        Double d = 0d;
+        for (int i = 0; i < this.capacity(); i++) {
+            d += Math.pow(this.get(i), p);
+        }
+        return Math.pow(d, (1/p));
+    }
 }
