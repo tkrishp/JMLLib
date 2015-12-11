608b8e1000e686d64ee8c994981b33ffc6081d2b
Tulasi Paradarami <research800@gmail.com>
2015-12-11 10:05:02 -0800

    add elastic net regularization option

--- src/main/java/com/tulc/optimization/options/Regularization.java
+++ src/main/java/com/tulc/optimization/options/Regularization.java
@@ -3,6 +3,7 @@ package com.tulc.optimization.options;
 public enum Regularization {
     L1("L1"),
     L2("L2"),
+    ELASTIC_NET("ELASTIC_NET"),
     NONE("NONE");
     
     private String regularization = "";
