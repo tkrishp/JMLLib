73d2ad31c8b7f58c44a5de1c2e6cf350d104dc57
Tulasi Paradarami <research800@gmail.com>
2016-02-19 11:26:59 -0800

    add class for vector of ints

---
+++ src/main/java/com/tulc/math/IVector.java
@@ -0,0 +1,34 @@
+package com.tulc.math;
+
+import java.util.Vector;
+
+public class IVector extends Vector<Integer> {
+    /**
+     * 
+     *
+    **/
+    private static final long serialVersionUID = -4506477794064248264L;
+    
+    public IVector() {
+        super();
+    }
+    
+    public IVector(int r) {
+        super(r);
+    }
+    
+    /**
+     * Returns the p-norm of the vector. 
+     * if p=1, Manahatten distance is returned (L1/Lasso)
+     * if p=2, Euclidean distance is returned (L2/Ridge)
+     * @param p p-norm of the vector
+     * @return norm of the vector
+     */
+    public Double pNorm(int p) {
+        Double d = 0d;
+        for (int i = 0; i < this.capacity(); i++) {
+            d += Math.pow(this.get(i), p);
+        }
+        return Math.pow(d, (1/p));
+    }   
+}
